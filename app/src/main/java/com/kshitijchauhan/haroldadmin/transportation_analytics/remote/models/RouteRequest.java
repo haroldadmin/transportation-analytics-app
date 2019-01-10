dd98d4371eb9d06de42c00af91bd2162424cdd54
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-01-10 14:22:42 +0530

    feat: Add Route Request Model

---
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/transportation_analytics/remote/models/RouteRequest.java
@@ -0,0 +1,61 @@
+package com.kshitijchauhan.haroldadmin.transportation_analytics.remote.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RouteRequest {
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("start_point_lat")
+    private double startPointLat;
+
+    @SerializedName("start_point_long")
+    private double startPointLong;
+
+    @SerializedName("end_point_lat")
+    private double endPointLat;
+
+    @SerializedName("end_point_long")
+    private double endPointLong;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getStartPointLat() {
+        return startPointLat;
+    }
+
+    public void setStartPointLat(double startPointLat) {
+        this.startPointLat = startPointLat;
+    }
+
+    public double getStartPointLong() {
+        return startPointLong;
+    }
+
+    public void setStartPointLong(double startPointLong) {
+        this.startPointLong = startPointLong;
+    }
+
+    public double getEndPointLat() {
+        return endPointLat;
+    }
+
+    public void setEndPointLat(double endPointLat) {
+        this.endPointLat = endPointLat;
+    }
+
+    public double getEndPointLong() {
+        return endPointLong;
+    }
+
+    public void setEndPointLong(double endPointLong) {
+        this.endPointLong = endPointLong;
+    }
+}
