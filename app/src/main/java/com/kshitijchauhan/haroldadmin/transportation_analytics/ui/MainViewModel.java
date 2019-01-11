d4d689f4b4681992a953043ae7cb36268fb8cb33
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-01-11 10:58:33 +0530

    feat: Setup material components dependency

---
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/transportation_analytics/ui/MainViewModel.java
@@ -0,0 +1,11 @@
+package com.kshitijchauhan.haroldadmin.transportation_analytics.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    public MainViewModel() {
+        super();
+    }
+
+}
