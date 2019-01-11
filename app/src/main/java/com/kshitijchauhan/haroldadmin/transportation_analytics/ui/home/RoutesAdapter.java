c9d60fe06c7a5e58b8d6826577cabca657f73fc8
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-01-11 23:12:13 +0530

    Add route maps list in home fragment

--- app/src/main/java/com/kshitijchauhan/haroldadmin/transportation_analytics/ui/home/RoutesAdapter.java
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/transportation_analytics/ui/home/RoutesAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.kshitijchauhan.haroldadmin.transportation_analytics.R;
 import com.kshitijchauhan.haroldadmin.transportation_analytics.models.RouteRequest;
 
 import androidx.annotation.NonNull;
@@ -12,15 +13,14 @@ import androidx.recyclerview.widget.ListAdapter;
 
 public class RoutesAdapter extends ListAdapter<RouteRequest, RoutesViewHolder> {
 
-    protected RoutesAdapter(@NonNull DiffUtil.ItemCallback<RouteRequest> diffCallback) {
+    public RoutesAdapter(@NonNull DiffUtil.ItemCallback<RouteRequest> diffCallback) {
         super(diffCallback);
     }
 
     @NonNull
     @Override
     public RoutesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout
-                .simple_list_item_1, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_route_request, parent, false);
         return new RoutesViewHolder(view);
     }
 
--- app/src/main/java/com/kshitijchauhan/haroldadmin/transportation_analytics/utilities/Constants.java
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/transportation_analytics/utilities/Constants.java
@@ -10,5 +10,5 @@ public class Constants {
 
     public static final String KEY_USER_ID = "user-id";
 
-    public static final String BASE_URL = "http://192.168.43.11:3000/api/";
+    public static final String BASE_URL = "http://192.168.1.219:3000/api/";
 }
